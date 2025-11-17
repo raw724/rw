9c45a06b32e20aa58908a043addbd61c082fbd29
raw724 <russellwalton99@icloud.com>
2025-11-17 14:54:17 +0000

    Revise About Me section for better readability
    Updated the 'About Me' section to improve clarity and flow.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,14 @@ export default function About() {
       <div className="glass p-8">
         <h3 className="text-2xl font-bold mb-4">About Me</h3>
         <p className="text-[--muted]">
-          I’m Russell — a Computer Science undergraduate at the University of Exeter with strong leadership experience managing teams and aiding staff in hospitality and water sports. Skilled and motivated to deliver outstanding customer experiences, able to take initiative in fast-paced environments. Driven, energetic, and passionate about community building, whilst exceeding expectations in everything I do.
+          Hello, I’m Russell — a Computer Science undergraduate at the University of Exeter with strong leadership experience managing teams and aiding staff in hospitality and water sports. 
+          Skilled and motivated to deliver outstanding customer experiences whilst also able to take initiative in fast-paced environments. 
+        </p>
+        <br></br>
+        <p className="text-[--muted]">
+          Me in three words? Driven, energetic, and passionate. With my experience as a school prefect throughout sixth form and my work as a Senior Sailing instructor alongside being Manager on Duty at Pizza Express,
+          I have experience in many situations. Being in two managerial roles proves my ability to work hard, motivate and support a team and represent a brand in every moment. Trusted in organizing and running sailing events and
+          school charity/parent events shows I can adapt to any scenario, work alongside a team with confidence and keep customers engaged effectively.
         </p>
       </div>
     </section>
